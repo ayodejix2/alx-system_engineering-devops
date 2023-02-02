6a6b1bbc3ae66dd80e17835374d3bd7a33153422
Ayodeji Ayodeji <110476398+ayodejix2@users.noreply.github.com>
2023-02-02 17:26:02 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-#Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-#List = [Format[0].compact, Format[1].compact, Format[2].compact]
-#puts List.join(',')
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
